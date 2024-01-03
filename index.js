d24e8839a6c1769af6744d29d42b2f4a2074df70
Alfian Prisma Yopiangga <51021806+yopiangga@users.noreply.github.com>
2024-01-03 15:32:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, World! q");
+  res.send("Hello, World! satu menit");
 });
 
 // Function to execute shell commands
